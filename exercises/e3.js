ca4fc681b9744eb7b8a77d2b2c4bc37545adae25
Gregory Gehrke <cayden.gehrke@gmail.com>
2025-10-22 12:04:55 -0700

    worked on e3

--- exercises/e3.js
+++ exercises/e3.js
@@ -1,8 +1,7 @@
-
 // EXERCISE 3
 // Make sure to solve two parts beneath
 
-/** 
+/**
  * PART 1
  * Return the average value for the given array of numbers.
  * Example: getAverage([22, 45, 4, 65]) => 34
@@ -11,21 +10,30 @@
 export function getAverage(array) {
   // Your code goes here...
 
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length;
 }
 
+const numbers = [22, 45, 4, 65];
+const average = getAverage(numbers);
+console.log(average);
 
-/** 
+/**
  * PART 2
  * Create a getStringSum(str) function that returns the sum of any integers that are in the string.
  * Example1: getStringSum("GH2U87A") => 17
  * Example2: getStringSum("GHIUJUHSG") => 0
- * */ 
+ * */
 
 export function getStringSum(str) {
   // Your code goes here...
-
+  for (let i = 0; i < str.length; i++) {
+    sum += str[i];
+  }
 }
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-3"
